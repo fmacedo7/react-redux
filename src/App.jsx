7e3a64e7729216e0d342584b3208be778902028f
Felizberto Macedo <felizberto11fred@gmail.com>
2022-12-23 23:21:34 -0300

    adicionei um novo coponente

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import React from "react";
+import "./App.css";
 import First from "./components/basics/First"; //o nome "First" n precisa ser exatamente o mesmo nome da funcao de origem
 import ComParametro from "./components/basics/ComParemetro";
+import Familia from "./components/basics/Familia";
+import FamiliaMembro from "./components/basics/FamiliaMembro";
 import Fragmento from "./components/basics/Fragmento";
 import Aleatorio from "./components/basics/Aleatorio";
 import Card from "./components/layout/Card";
 
 export default (_) => (
-  <div id="app">
+  <div className="App">
     <h1>Fundamentos React</h1>
+    <div className="Cards">
+      <Card titulo="#05 - Familia" color="#CD7F32">
+        <Familia sobrenome="Novachronos">
+          <FamiliaMembro nome="Alice" />
+          <FamiliaMembro nome="Felizberto" />
+          <FamiliaMembro nome="Licht" />
+        </Familia>
+      </Card>
 
-    <Card titulo="#04 - Aleatorio">
-      <Aleatorio min={1} max={60} />
-    </Card>
+      <Card titulo="#04 - Aleatório" color="#CD7F32">
+        <Aleatorio min={1} max={60} />
+      </Card>
 
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
+      <Card titulo="#03 - Fragmento" color="#954535">
+        <Fragmento />
+      </Card>
 
-    <Card titulo="#02 - Com Paramentro">
-      <ComParametro titulo="Situcao do aluno" aluno="Ádila" nota={9.9} />
-    </Card>
+      <Card titulo="#02 - Com Paramentro" color="#A52A2A">
+        <ComParametro titulo="Situação do aluno" aluno="Ádila" nota={9.9} />
+      </Card>
 
-    <Card titulo="#01 - First">
-      <First></First>
-    </Card>
+      <Card titulo="#01 - First" color="#D27D2D">
+        <First></First>
+      </Card>
+    </div>
   </div>
 );
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -1,9 +1,15 @@
-import "./Card.css"
+import "./Card.css";
 import React from "react";
 
 export default (props) => {
+  
+  const styleCard = {
+    backgroundColor: props.color || '#F00',
+    borderColor: props.color || '#F00',
+  };
+
   return (
-    <div className="Card">
+    <div className="Card" style={styleCard}>
       <div className="Title"> {props.titulo} </div>
       <div className="Content"> {props.children} </div>
     </div>
